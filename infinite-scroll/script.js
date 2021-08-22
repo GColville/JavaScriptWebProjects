84d8586f35d452afa58b813fc6038f0c6224f238
WannaBeDev <16037758+GColville@users.noreply.github.com>
2021-08-22 13:35:54 +0100

    completed the Infinite Scroller project.
     For privacy sake I am removing the API key I have for Unsplash.

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -1,12 +1,24 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
-const apiKey = 'UB5NKpRl66NUhngl-2kAXgJPMYC7xfo3PulMb4itMmQ';
+const count = 30;
+const apiKey = 'INSERT PUBLIC API KEY';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
 
 //  helper function to set attributes on DOM elements
 function setAttributes(element, attributes) {
@@ -17,6 +29,8 @@ function setAttributes(element, attributes) {
 
 // Create elements for links & photos, add to the DOM
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     // run function for each object in photosArray
     photosArray.forEach((photo) => {
         // create <a> to link to unsplash
@@ -33,6 +47,8 @@ function displayPhotos() {
             alt: photo.alt_description,
             title: photo.alt_description,
         });
+        // event listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
         // put <img> inside <a>, then put both inside imageContainer element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -49,6 +65,14 @@ async function getPhotos() {
         // catch error here
     }
 }
+
+// check to see if scrolling near bottom of page, load more photos
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+});
 
 // on load
 getPhotos();
